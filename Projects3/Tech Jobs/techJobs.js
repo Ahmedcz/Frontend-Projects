cae94f45da44b61155b1a274e2c724533e3dd418
Ahmedcz <144346736+Ahmedcz@users.noreply.github.com>
2024-03-01 00:27:19 +0100

    Tech Jobs Project
    Creating a job board / integrating google maps API for location. The project will be updated

---
+++ Projects3/Tech Jobs/techJobs.js
@@ -0,0 +1,42 @@
+// Not needed in a static maps view using IFrame in HTML
+/*
+ function initMap() {
+    fetch('')
+    .then(response => {
+        if(!response.ok){
+            throw new OverconstrainedError ('Network Response was not ok !');
+        }
+
+        return response.json;
+    })
+    
+    .then(data => {
+        const map = new.google.maps.Map(document.getElementById('map'), {
+            center: { lat: data.latitude, lng: data.latitude },
+            zoom: data.zoomLevel,
+        });
+    });
+
+   .catch(error => {
+    console.error('Error initializing Map: ', error);
+   });
+} 
+
+
+
+<script>
+  function initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 50.04767032834682, lng: 14.355072443220774 },
+      zoom: 15,
+    });
+
+    const marker = new google.maps.Marker({
+      position: { lat: 50.04767032834682, lng: 14.355072443220774 },
+      map: map,
+      icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', // URL to your custom marker icon
+    });
+  }
+</script>
+
+*/
